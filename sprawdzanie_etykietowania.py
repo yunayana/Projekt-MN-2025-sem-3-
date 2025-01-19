837832d9299037cccf940b0730e3c7b23a74db3f
Yana Trotsenko <164183176+yunayana@users.noreply.github.com>
2025-01-19 23:47:20 +0100

    Update sprawdzanie_etykietowania.py

--- sprawdzanie_etykietowania.py
+++ sprawdzanie_etykietowania.py
@@ -8,7 +8,6 @@ def load_json(json_file):
     with open(json_file, 'r') as f:
         return json.load(f)
 
-# Funkcja do wizualizacji granic obiektów na obrazie
 def visualize_borders(image_path, annotations):
     # Wczytaj obraz za pomocą OpenCV
     image = cv2.imread(image_path)
@@ -17,33 +16,27 @@ def visualize_borders(image_path, annotations):
     fig, ax = plt.subplots()
     ax.imshow(image)
     
-    # Przejdź przez obiekty i narysuj granice
+    # przechodzenie przez obiekty i rysowanie granic
     for annotation in annotations:
         class_name = annotation['class']
         bbox = annotation['bbox']
         x1, y1, x2, y2 = bbox
-        
-        # Narysuj prostokąt na obrazie
+ 
         ax.add_patch(Rectangle((x1, y1), x2 - x1, y2 - y1, linewidth=2, edgecolor='r', facecolor='none'))
-        
-        # Opcjonalnie dodaj nazwę klasy obiektu
         ax.text(x1, y1 - 10, class_name.strip(), color='red', fontsize=12, backgroundcolor='white')
-    
-    # Wyświetl obraz z nałożonymi granicami
     plt.show()
 
-# Funkcja do sprawdzenia i wizualizacji granic dla wszystkich zdjęć w folderze
+# funkcja sprawdzenia i wizualizacji granic na zdjeciach w folderze
 def check_borders(json_file, images_folder):
-    # Wczytaj dane z pliku JSON
     data = load_json(json_file)
     
-    # Sprawdź każdy obraz i narysuj granice
+    #sprawdzenie kazdego obrazu i rysowanie granicy
     for entry in data:
-        image_name = entry.get('image')  # Nazwa obrazu w danych JSON
+        image_name = entry.get('image')  
         if image_name:
             image_path = os.path.join(images_folder, image_name)
             if os.path.exists(image_path):
-                annotations = entry.get('annotations', [])  # Lista obiektów
+                annotations = entry.get('annotations', []) 
                 visualize_borders(image_path, annotations)
             else:
                 print(f"Obraz {image_name} nie istnieje w folderze.")
@@ -51,7 +44,7 @@ def check_borders(json_file, images_folder):
             print("Brak nazwy obrazu w danych JSON.")
 
 # Przykładowe użycie:
-json_file = 'etykieta.json'  # Podaj ścieżkę do pliku JSON
-images_folder = 'Foto'  # Podaj ścieżkę do folderu z obrazami
+json_file = 'etykieta.json' 
+images_folder = 'Foto' 
 
 check_borders(json_file, images_folder)
